f57412a8fe4e63ce9664ba86933e6b7900550c64
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-04-01 13:59:38 -0300

    feat/Se hicieron las funciones render

---
+++ src/views/about.js
@@ -0,0 +1,6 @@
+export function renderContact() {
+    const app = document.querySelector('.app');
+    app.innerHTML = `
+        <h1>Contactos</h1>
+        <p>Contactanos ante cualquier consulta o inquietud</p>`
+}
---
+++ src/views/chat.js
@@ -0,0 +1,5 @@
+export function renderChat() {
+    const app = document.querySelector('.app');
+    app.innerHTML = `
+        <h1>Chat Bot</h1>`
+}
