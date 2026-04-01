0815f6e99a90494854c07474e6594362a56b339c
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-04-01 14:23:40 -0300

    feat/export function to better navigation

---
+++ src/script.js
@@ -0,0 +1,13 @@
+import { router } from "./router.js";
+import { setupLinkInterception } from "./navigation.js";
+
+// Listener for back/forward navigation
+window.addEventListener('popstate', () =>  {
+    router();
+});
+
+// Intercept link clicks
+setupLinkInterception();
+
+// Render the initial route
+router();
