dcf4a0be3662dd6f6142b455656104fc935f874c
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-04-01 15:34:31 -0300

    feat/style and structure to chat.js

--- src/views/chat.js
+++ src/views/chat.js
@@ -1,8 +1,26 @@
 export function renderChat() {
     const app = document.querySelector('.app');
     app.innerHTML = `
-    <main class="chat-container">
-        <h2>Chat Bot</h2>
-    </main>
+    <main class="chat">
+                <!-- Header del chat -->
+                <div class="chat__header">
+                    <div class="chat__user">
+                    <img src="avatar.jpg" alt="Usuario" class="chat__avatar">
+                    <span class="chat__name">Juan Pérez</span>
+                    </div>
+                </div>
+                <!-- Mensajes -->
+                <div class="chat__messages">
+                </div>
+                <!-- Input -->
+                <div class="chat__input">
+                    <input 
+                    type="text" 
+                    class="chat__field" 
+                    placeholder="Escribí un mensaje..."
+                    >
+                    <button class="chat__button">Enviar</button>
+                </div>
+            </main>
         `
 }
