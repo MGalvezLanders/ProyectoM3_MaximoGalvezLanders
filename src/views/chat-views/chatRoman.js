f64338e7cad316feae279266220f0a0d1df1efb8
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-04-02 21:09:07 -0300

    feat/new folder chat-views with the chat of all the characters

---
+++ src/views/chat-views/chatRoman.js
@@ -0,0 +1,26 @@
+const chatContainer = document.querySelector('#chat');
+
+export function renderChatToretto() {
+    if (!chatContainer) return;
+    chatContainer.innerHTML = `
+            <!-- Header del chat -->
+            <div class="chat__header">
+                <div class="chat__user">
+                <img src="${window.location.origin}/src/img/roman.webp" alt="Usuario" class="chat__avatar">
+                <span class="chat__name">Roman Pearce</span>
+                </div>
+            </div>
+            <!-- Mensajes -->
+            <div class="chat__messages">
+            </div>
+            <!-- Input -->
+            <div class="chat__input">
+                <input 
+                type="text" 
+                class="chat__field" 
+                placeholder="Escribí un mensaje..."
+                >
+                <button class="chat__button">Enviar</button>
+            </div>
+        `;
+}
