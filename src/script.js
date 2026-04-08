765e17fcbf7c62ad2356f4ad89812a4048b20795
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-04-08 18:48:57 -0300

    fix/add vite config

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
 import { router, navigateTo } from "./routes/router.js";
 import { setupLinkInterception } from "./navigation/navigation.js";
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist'
+  }
+});
 
 // Listener for back/forward navigation
 window.addEventListener('popstate', () =>  {
